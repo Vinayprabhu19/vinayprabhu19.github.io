7c11141a44378dd517fa8a542e56dd64c154ad6e
vp19 <vinayprabhu19@gmail.com>
2020-05-11 19:57:04 +0530

    revamp

--- js/script.js
+++ js/script.js
@@ -6,53 +6,15 @@ $(document).ready(function(){
 	//check1();
 	moveAround();
 })
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
-
+document.addEventListener('swiped-right', function(e) {
+    moveLeft(); // the element that was swiped
+});
+document.addEventListener('swiped-left', function(e) {
+    moveRight(); // the element that was swiped
+});
 var xDown = null;                                                        
 var yDown = null;
 
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */ 
-			moveLeft();
-        } else {
-            /* right swipe */
-			moveRight();
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */ 
-			moveUp();
-        } else { 
-            /* down swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
 function moveAround(){
 	
 	$(document).on('keydown',function(e){
